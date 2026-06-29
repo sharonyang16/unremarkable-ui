a9d6e392377b5e926210968aefe59c748e53c762
Sharon Yang <sharonyyang0@gmail.com>
2026-06-28 21:43:12 -0400

    fix leading

--- components/Typography/Typography.variants.ts
+++ components/Typography/Typography.variants.ts
@@ -1,18 +1,18 @@
 import { cva } from 'class-variance-authority';
 
-export const typographyVariants = cva('leading-[1.25]', {
+export const typographyVariants = cva('', {
   variants: {
     variant: {
-      h1: 'font-semibold text-[64px] font-serif',
-      h2: 'font-semibold text-[48px] font-serif',
-      h3: 'font-semibold text-[36px] font-serif',
-      h4: 'font-semibold text-xl',
-      h5: 'font-semibold text-lg',
-      h6: 'font-semibold text-base',
-      label1: 'font-medium text-base',
-      label2: 'font-medium text-sm',
-      body1: 'font-normal text-base',
-      body2: 'font-normal text-sm',
+      h1: 'font-semibold text-[64px] font-serif leading-[1.25]',
+      h2: 'font-semibold text-[48px] font-serif leading-[1.25]',
+      h3: 'font-semibold text-[36px] font-serif leading-[1.25]',
+      h4: 'font-semibold text-xl leading-[1.25]',
+      h5: 'font-semibold text-lg leading-[1.25]',
+      h6: 'font-semibold text-base leading-[1.25]',
+      label1: 'font-medium text-base leading-[1.25]',
+      label2: 'font-medium text-sm leading-[1.25]',
+      body1: 'font-normal text-base leading-[1.25]',
+      body2: 'font-normal text-sm leading-[1.25]',
     },
     color: {
       primary: 'text-black dark:text-white',
