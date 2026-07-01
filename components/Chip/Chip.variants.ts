412b774d293a2d45e446a314f3b09786a583ed6a
Sharon Yang <sharonyyang0@gmail.com>
2026-07-01 13:35:47 -0400

    use text consts for Chip

--- components/Chip/Chip.variants.ts
+++ components/Chip/Chip.variants.ts
@@ -1,18 +1,17 @@
 import { cva } from 'class-variance-authority';
-import { cn } from '../utils';
-import { typographyVariants } from '../Typography/Typography.variants';
+import { textSizes, textColors } from '../BaseStyles';
 
 export const chipVariants = cva('inline-flex items-center rounded-full w-fit', {
   variants: {
     variant: {
-      solid: 'bg-black dark:bg-white',
-      subtle: 'bg-gray-200 dark:bg-gray-800',
-      outline: 'border-1 border-black dark:border-white',
+      solid: `${textColors.inverse} bg-black dark:bg-white `,
+      subtle: `${textColors.primary} bg-gray-200 dark:bg-gray-800`,
+      outline: `${textColors.primary} border-1 border-black dark:border-white`,
     },
     size: {
-      sm: cn('py-0.5 px-2', typographyVariants({ variant: 'body2' })),
-      md: 'py-1 px-2.5',
-      lg: 'py-1.5 px-3',
+      sm: `py-0.5 px-2 ${textSizes.body2}`,
+      md: `py-1 px-2.5 ${textSizes.body2}`,
+      lg: `py-1.5 px-3 ${textSizes.body1}`,
     },
   },
   defaultVariants: {
